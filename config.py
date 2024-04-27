6d08a11bf968e0ba1ae1f47b391f262589971d18
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-04-28 00:29:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=1619182628
-    API_HASH="ksjs"
-    TOKEN="jaksks"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "5264285143").split(" "))
-    START_IMG="jdjsjw"
-    BOT_ID=1013
-    BOT_USERNAME="djsjw"
-    BOT_NAME="BAN ALL"
+    API_ID="24089031"
+    API_HASH="0615e3afe13ddaaf8e9ddbd3977d35ff"
+    TOKEN="7114018519:AAE2w9ylyJkUN9rFkLSwWr6BcUYxsIagFBo"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "5702598840").split("2010819209"))
+    START_IMG="https://telegra.ph/file/6035b42dee58754328577.jpg"
+    BOT_ID=7114018519
+    BOT_USERNAME="Tgmpbrhbanalltgmpbrhbot"
+    BOT_NAME="Musić Downloader"
